7de5787425f4ea8aad9dbee34b1349a5ee7c42bb
PurtucRobert <purtuc.robert@gmail.com>
2022-06-02 12:16:16 +0300

    Added the hall model

--- cinemy/cinema/admin.py
+++ cinemy/cinema/admin.py
@@ -2,10 +2,19 @@ from django.contrib import admin
 from cinema.models import Movie, Cinema, Hall
 
 
+@admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
     exclude = ("imdb_link",)
 
 
-admin.site.register(Movie, MovieAdmin)
-admin.site.register(Cinema)
+class HallInline(admin.TabularInline):
+    model = Hall
+    extra = 0
+
+
+@admin.register(Cinema)
+class CinemaAdmin(admin.ModelAdmin):
+    inlines = (HallInline,)
+
+
 admin.site.register(Hall)
