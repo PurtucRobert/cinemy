b950d0187a38d3f59348c535202b0b22f2b18b74
PurtucRobert <purtuc.robert@gmail.com>
2022-06-10 11:00:11 +0300

    Added unit tests for login and logout

---
+++ cinemy/login/tests.py
@@ -0,0 +1,23 @@
+from django.test import TestCase
+from django.contrib.auth import login
+from django.contrib.auth import authenticate
+from django.contrib.auth import get_user_model
+
+
+class AuthenticationTest(TestCase):
+    def test_login(self):
+        User = get_user_model()
+        user = User.objects.create_user("temporary", "temporary@gmail.com", "temporary")
+        self.client.login(username="temporary", password="temporary")
+        response = self.client.get("/", follow=True)
+        self.assertEqual(
+            response.context["user"].username, "temporary", "Login has failed"
+        )
+
+    def test_logout(self):
+        User = get_user_model()
+        user = User.objects.create_user("temporary", "temporary@gmail.com", "temporary")
+        self.client.login(username="temporary", password="temporary")
+        self.client.logout()
+        response = self.client.get("")
+        self.assertTrue("Login" in str(response.content), "Logout has failed")
