88d52df49a35fbbe124e7f324f4fb8c0f680defc
PurtucRobert <purtuc.robert@gmail.com>
2022-06-23 14:48:08 +0300

    Added the occupied field in seat model

--- cinemy/cinema/models.py
+++ cinemy/cinema/models.py
@@ -54,6 +54,7 @@ class Seat(models.Model):
         Hall, on_delete=models.CASCADE, related_name="seats", null=True
     )
     name = models.CharField(max_length=4, default="A1")
+    occupied = models.BooleanField(default=False)
 
     def __str__(self):
         return format_html(f"Seat: {self.name}<br>Hall: {str(self.hall)}")
