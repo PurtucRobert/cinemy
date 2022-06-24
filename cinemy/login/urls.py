d041f17db62d9f940a238b33f23ae6e2b51958f2
PurtucRobert <purtuc.robert@gmail.com>
2022-06-24 13:23:18 +0300

    The generated token is in base64 format

--- cinemy/login/urls.py
+++ cinemy/login/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
+from login.viewsets import obtain_auth_token_base64
 from login.views import signup, auth, pre_signup
 from django.contrib.auth.views import LogoutView
-from rest_framework.authtoken import views
+
 
 urlpatterns = [
     path("login/", auth, name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
-    path("pre_signup/", pre_signup, name="pre_signup"),
+    path("pre-signup/", pre_signup, name="pre_signup"),
     path("signup/<slug:uidb64>/", signup, name="signup"),
-    path("generate_api_token/", views.obtain_auth_token),
+    path("generate-api-token/", obtain_auth_token_base64),
 ]
